cccc43d6ace026e6ecf75dbb66815f9db2cd78f7
farizhaidar <farizhaidar00@gmail.com>
2024-05-27 17:46:46 +0700

    sheess

--- app/src/main/java/com/farizz/lafyuu/SplashActivity.kt
+++ app/src/main/java/com/farizz/lafyuu/SplashActivity.kt
@@ -17,6 +17,6 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000) // 3000 milidetik = 3 detik
+        }, 2000)
     }
 }
