9cc993ba1ac64ffc14a71bce9e15a8869b35eaf0
farizhaidar <farizhaidar00@gmail.com>
2024-05-27 17:28:16 +0700

    lafyuu

---
+++ app/src/main/java/com/farizz/lafyuu/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.farizz.lafyuu
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Timer untuk splash screen
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000) // 3000 milidetik = 3 detik
+    }
+}
